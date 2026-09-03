88174a3597a477ad48365ebfbe1b0a9259ffc37b
Aditya Raj Sharma <adityarajsharma3030@gmail.com>
2026-09-02 19:10:09 -0500

    added a function to extract video data using the video ids

--- video_stats.py
+++ video_stats.py
@@ -71,8 +71,63 @@ def get_video_ids(playListId):
 
     except requests.exceptions.RequestException as e:
         raise e
+    
+
+
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    # we need a helper function to be able to add 50 video_ids comma separated in API request for URL
+
+    def batch_list(video_id_list, batch_size):
+        # yield helps return multiple batches one after the other
+        for video_id in range(0, len(video_id_list), batch_size):
+            yield video_id_list[video_id: video_id + batch_size]
+
+    
+
+    try:
+        # getting a list of video_ids in batches of 50
+        for batch in batch_list(video_ids, maxResults):
+            # joining them separating them by a comma into a single string and using the video_ids_str in the url
+            video_ids_str = ",".join(batch)
+            
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}"
+
+            # getting a json response - this will give a response for 50 videos
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            # for each item inside data['items'] - each item represents data for 1 video so we loop over each item to get specific data for each video
+            for item in data.get("items", []):
+                # assign variables to get smaller sections of the json
+                video_id = item["id"]
+                snippet = item["snippet"]
+                contentDetails = item["contentDetails"]
+                statistics = item["statistics"]
+                
+                # creating a video_data dictionary to get specific data for each video and appending into extracted data list
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet["title"],
+                    "publishedAt": snippet['publishedAt'],
+                    "duration": contentDetails["duration"],
+                    "viewCount": statistics.get("viewCount", None),
+                    "likeCount": statistics.get("likeCount", None),
+                    "commentCount": statistics.get("commentCount", None)            
+                }
+
+                extracted_data.append(video_data)
+        # extracted data list will have dictionaries containing specific data for all videos
+        return extracted_data
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
 
 if __name__ == "__main__":
     playListId = get_playList_id() 
-    get_video_ids(playListId)
+    video_ids = get_video_ids(playListId)
+    extract_video_data(video_ids)
     
