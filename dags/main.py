f5f8283edf7a1f9bb612e27873f99a00b259d39f
Aditya Raj Sharma <adityarajsharma3030@gmail.com>
2026-09-22 22:51:24 -0500

    Add Airflow data warehouse pipeline and Soda quality checks

--- dags/main.py
+++ dags/main.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from api.video_stats import get_playList_id, get_video_ids, extract_video_data, save_to_json
 
 from datawarehouse.dwh import staging_table, core_table
+from dataquality.soda import yt_elt_data_quality
 
 # define the local timezone
 local_tz = pendulum.timezone("America/Chicago")
@@ -22,6 +23,11 @@ default_args = {
     "start_date": datetime(2025, 1, 1, tzinfo=local_tz),
     # 'end_date': datetime(2030, 12, 31, tzinfo=local_tz),
 }
+
+# Variables for staging and core schemas
+staging_schema = "staging"
+core_schema = "core"
+
 
 with DAG(
     dag_id = "produce_json",
@@ -55,3 +61,18 @@ with DAG(
     
     # Define dependencies (in the order of execution)
     update_staging >> update_core
+
+with DAG(
+    dag_id = "data_quality",
+    default_args=default_args,
+    description="DAG to check the data quality on both layers in the db",
+    schedule = "0 16 * * *",
+    catchup=False
+) as dag:
+    
+    # Define tasks
+    soda_validate_staging = yt_elt_data_quality(staging_schema)
+    soda_validate_core= yt_elt_data_quality(core_schema)
+    
+    # Define dependencies (in the order of execution)
+    soda_validate_staging >> soda_validate_core
